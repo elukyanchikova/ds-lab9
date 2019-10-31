a1012ddf427d4869cf770462c00d4cb27134266d
elukyanchikova <31773897+elukyanchikova@users.noreply.github.com>
2019-11-01 00:21:51 +0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+import pymongo
+
+app = Flask(__name__)
+db_client = pymongo.MongoClient('mongodb://18.188.243.182:27017')
+db = db_client['chat']
+
+
+@app.route('/')
+def main_page():
+	return render_template('index.html', messages=get_messages())
+
+@app.route('/', methods=['POST'])
+def get_form():
+	message = request.form['Message']
+	tudor = request.form['Tudor']
+
+	collection = db['messages']
+	collection.insert_one({'tudor': tudor, 'content': message})
+
+	return render_template('index.html', messages=get_messages())
+
+def get_messages():
+	collection = db['messages']
+	messages = collection.find({}, {'_id': 0})
+	return messages
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
