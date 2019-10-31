f8b17150e831e34140415854badc22c3d08ccb05
elukyanchikova <31773897+elukyanchikova@users.noreply.github.com>
2019-11-01 00:37:58 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,20 +5,21 @@ app = Flask(__name__)
 db_client = pymongo.MongoClient('mongodb://18.188.243.182:27017')
 db = db_client['chat']
 
-
 @app.route('/')
 def main_page():
 	return render_template('index.html', messages=get_messages())
 
+
 @app.route('/', methods=['POST'])
 def get_form():
 	message = request.form['Message']
-	tudor = request.form['Tudor']
+	tudor = request.form['Author']
 
 	collection = db['messages']
-	collection.insert_one({'tudor': tudor, 'content': message})
+	collection.insert_one({'author': tudor, 'content': message})
 
 	return render_template('index.html', messages=get_messages())
+
 
 def get_messages():
 	collection = db['messages']
